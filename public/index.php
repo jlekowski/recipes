cc08f5822a7d7d30996f9b0bc8510140199b8777
Jerzy Lekowski <jerzy@lekowski.pl>
2014-08-07 22:06:34 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Slim\Slim;
+
+$app = new Slim(array(
+    'debug' => true,
+    'templates.path' => '../templates',
+    'mode' => 'development',
+//    'log.writer' => new \My\LogWriter(),
+//    'log.level' => \Slim\Log::DEBUG,
+//    'log.enabled' => true,
+//    'view' => new \My\View(),
+    ''
+));
+
+//$app->setName('recipes');
+//$app->get('/foo', function () use ($app) {
+//    $app->render('foo.php'); // <-- SUCCESS
+//});
+
+//// Only invoked if mode is "production"
+//$app->configureMode('production', function () use ($app) {
+//    $app->config(array(
+//        'log.enable' => true,
+//        'debug' => false
+//    ));
+//});
+//
+//// Only invoked if mode is "development"
+//$app->configureMode('development', function () use ($app) {
+//    $app->config(array(
+//        'log.enable' => false,
+//        'debug' => true
+//    ));
+//});
+
+\Slim\Route::setDefaultConditions(array(
+    'id' => '\d+'
+));
+
+$app->get('/', function() {
+    echo "Recipes";
+});
+
+$app->get('/recipe/:id', function($id) use ($app) {
+    echo "Recipe: $id";
+    echo "<br>";
+//    $app->flash('error', 'Login required');
+    echo $app->urlFor('recipe2', array('id' => 999));
+})->name('recipe2');
+
+$app->post('/recipe/:id', function($id) {
+    echo "Recipe updated: $id";
+});
+
+$app->run();
